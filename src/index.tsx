c860bd4950061a4f7fec6d9ea990abd9ebc3d7bf
lawson <abraham.lawson.dev@gmail.com>
2021-07-27 22:27:37 +0700

    [master] add type

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import { NativeModules } from 'react-native';
 
 type WallpaperManagerType = {
-  setWallpaper(url: string, type: number): Promise<number>;
+  setWallpaper(url: string, type: number): Promise<string>;
+};
+
+type Type = {
+  FLAG_LOCK: number;
+  FLAG_SYSTEM: number;
 };
 
 const { WallpaperManager } = NativeModules;
+
+export const TYPE: Type = WallpaperManager.getConstants();
 
 export default WallpaperManager as WallpaperManagerType;
